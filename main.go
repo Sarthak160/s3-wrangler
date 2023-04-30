bb141c40872411604c6fb6e2472d9ddd33b60028
Sarthak Shyngle <50234097+Sarthak160@users.noreply.github.com>
2023-04-30 18:08:17 +0530

    third commit

--- main.go
+++ main.go
@@ -1,82 +1,131 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "io"
-    
-    "github.com/aws/aws-sdk-go/aws"
-    "github.com/aws/aws-sdk-go/aws/session"
-    "github.com/aws/aws-sdk-go/service/s3"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
 )
 
 func main() {
-    // Create a new AWS session
-    sess := session.Must(session.NewSession(&aws.Config{
-    Region: aws.String("us-west-2"),
-}))
+	bucket := "keploy-enterprise"
+	key := "keploy/io/0.1.0/enterprise-server_windows_amd64.tar.gz"
+	region := "us-west-2"
 
+	url := fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", bucket, region, key)
 
-    // Create a new S3 client
-    svc := s3.New(sess)
+	// Get the object using a simple HTTP GET request
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error getting object:", err)
+		return
+	}
 
-    // Set the S3 bucket and object key
-    bucket := "keploy-enterprise"
-    key := "keploy/io/0.1.0/enterprise-server_windows_amd64.tar.gz"
+	defer resp.Body.Close()
 
-    // Create a new GetObjectInput
-    input := &s3.GetObjectInput{
-        Bucket: aws.String(bucket),
-        Key:    aws.String(key),
-    }
+	// Check if the object was retrieved successfully
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("Error:", resp.Status)
+		return
+	}
 
-    // Use the S3 client to download the object
-    result, err := svc.GetObject(input)
-    if err != nil {
-        fmt.Println("Error downloading object:", err)
-        return
-    }
-    defer result.Body.Close()
+	// Save the object to a local file
+	file, err := os.Create("./enterprise-server_windows_amd64.tar.gz")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
 
-    // Create a new file to save the object content to
-    file, err := os.Create("enterprise-server_windows_amd64.tar.gz")
-    if err != nil {
-        fmt.Println("Error creating file:", err)
-        return
-    }
-    defer file.Close()
+	defer file.Close()
 
-    // Save the object content to the file
-    _, err = io.Copy(file, result.Body)
-    if err != nil {
-        fmt.Println("Error saving object content to file:", err)
-        return
-    }
+	if _, err := io.Copy(file, resp.Body); err != nil {
+		fmt.Println("Error writing object to file:", err)
+		return
+	}
 
-    fmt.Println("File downloaded successfully!")
+	fmt.Println("Successfully downloaded object to", key)
 }
+
 
 // package main
 
 // import (
-// 	"fmt"
-// 	"regexp"
-// 	"strings"
+//     "fmt"
+//     "os"
+//     "io"
+    
+//     "github.com/aws/aws-sdk-go/aws"
+//     "github.com/aws/aws-sdk-go/aws/session"
+//     "github.com/aws/aws-sdk-go/service/s3"
 // )
 
 // func main() {
-// 	email := "Cample@xample.com"
-// 	email = strings.ToLower(email)
-// 	// regular expression pattern to match email addresses
-// 	pattern := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
-	
-// 	// compile the pattern into a regular expression object
-// 	regex := regexp.MustCompile(pattern)
-	
-// 	// check if the email matches the pattern
-// 	if regex.MatchString(email) {
-// 		fmt.Println("Email is valid")
-// 	} else {
-// 		fmt.Println("Email is invalid")
-// 	}
+//     // Create a new AWS session
+//     sess := session.Must(session.NewSession(&aws.Config{
+//     Region: aws.String("us-west-2"),
+// }))
+
+
+//     // Create a new S3 client
+//     svc := s3.New(sess)
+
+//     // Set the S3 bucket and object key
+//     bucket := "keploy-enterprise"
+//     key := "keploy/io/0.1.0/enterprise-server_windows_amd64.tar.gz"
+
+//     // Create a new GetObjectInput
+//     input := &s3.GetObjectInput{
+//         Bucket: aws.String(bucket),
+//         Key:    aws.String(key),
+//     }
+
+//     // Use the S3 client to download the object
+//     result, err := svc.GetObject(input)
+//     if err != nil {
+//         fmt.Println("Error downloading object:", err)
+//         return
+//     }
+//     defer result.Body.Close()
+
+//     // Create a new file to save the object content to
+//     file, err := os.Create("enterprise-server_windows_amd64.tar.gz")
+//     if err != nil {
+//         fmt.Println("Error creating file:", err)
+//         return
+//     }
+//     defer file.Close()
+
+//     // Save the object content to the file
+//     _, err = io.Copy(file, result.Body)
+//     if err != nil {
+//         fmt.Println("Error saving object content to file:", err)
+//         return
+//     }
+
+//     fmt.Println("File downloaded successfully!")
 // }
+
+// // package main
+
+// // import (
+// // 	"fmt"
+// // 	"regexp"
+// // 	"strings"
+// // )
+
+// // func main() {
+// // 	email := "Cample@xample.com"
+// // 	email = strings.ToLower(email)
+// // 	// regular expression pattern to match email addresses
+// // 	pattern := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+	
+// // 	// compile the pattern into a regular expression object
+// // 	regex := regexp.MustCompile(pattern)
+	
+// // 	// check if the email matches the pattern
+// // 	if regex.MatchString(email) {
+// // 		fmt.Println("Email is valid")
+// // 	} else {
+// // 		fmt.Println("Email is invalid")
+// // 	}
+// // }
